773f5b140539eb0896e2fcf958bdfc5b25504fe3
asweetcake <beka.murat.2021@gmail.com>
2025-06-28 21:36:33 +0500

    Added finished phonebook step 2 exercise

--- part2/2.6-2.17/phonebook/src/App.jsx
+++ part2/2.6-2.17/phonebook/src/App.jsx
@@ -1,20 +1,20 @@
 import { useState } from 'react'
 
-const Phonebook = ({title, onInputChange, onAdd}) => {
+const Phonebook = ({title, onInputChange, onAdd, value}) => {
   return(
     <>
       <h2>{title}</h2>
-      <Form onInputChange={onInputChange} onAdd={onAdd}/>
+      <Form onInputChange={onInputChange} onAdd={onAdd} value={value}/>
     </>
   )
 }
 
-const Form = ({onInputChange, onAdd}) => {
+const Form = ({onInputChange, onAdd, value}) => {
   return(
     <>
       <form>
         <div>
-          name: <Input onChange={onInputChange}/>
+          name: <Input onChange={onInputChange} value={value}/>
         </div>
         <div>
           <Button title={"add"} type={"submit"} onClick={onAdd}/>
@@ -24,12 +24,12 @@ const Form = ({onInputChange, onAdd}) => {
   )
 }
 
-const Input = ({onChange}) => <input onChange={onChange}/>
+const Input = ({onChange, value}) => <input value={value} onChange={onChange}/>
 const Button = ({title, type, onClick}) => <button type={type} onClick={onClick}>{title}</button>
 
 const ShowPerson = ({persons}) => 
   <div>
-    {persons.map((person, index) => <p key={index}>{person.name}</p>)}
+    {persons.map(person => <p key={person.name}>{person.name}</p>)}
   </div>
 
 const App = () => {
@@ -45,17 +45,25 @@ const App = () => {
 
   const handleAddPerson = (event) => {
     event.preventDefault()
-    const newPerson = {
-      name: newName
-    }
-    setPersons(persons.concat(newPerson))
-    setNewName('')
+    if (newName.trim() === ""){
+      console.log("Name cannot be empty")
+      return
+    }  
+
+    const personExist = persons.some(person => person.name === newName)
+    
+    personExist 
+    ? alert(`${newName} is already added to phonebook`) 
+    : (
+        setPersons(persons.concat({name: newName})),
+        setNewName('')
+      ) 
   }
   console.log("persons:", persons)
   
   return (
     <div>
-      <Phonebook title={"Phonebook"} onInputChange={handleInput} onAdd={handleAddPerson}/>
+      <Phonebook title={"Phonebook"} onInputChange={handleInput} onAdd={handleAddPerson} value={newName}/>
       <h2>Numbers</h2>
       <ShowPerson persons={persons}/>
     </div>
