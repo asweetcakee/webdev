2799d8fdf94bd4e9a4a3d0eae8428eacd48fbfc2
asweetcake <beka.murat.2021@gmail.com>
2025-07-05 17:40:57 +0500

    Completed ex. 3.8: phonebook backend step 8. Implemented custom morgan logger based on tiny.

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
-
 const morgan = require('morgan')
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+const tiny = ':method :url :status :res[content-length] - :response-time ms'
+morgan.token('body', (req, res) => {
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan(`${tiny} :body`))
+
 
 let persons = [
     { 
