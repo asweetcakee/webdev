81304738f75cae65ec2ca6623478e38eb5c97a98
asweetcake <beka.murat.2021@gmail.com>
2025-07-21 21:24:22 +0500

    Add * to force LF line endings using .gitattributes

--- part3/phonebook/frontend/src/components/Button.jsx
+++
@@ -1,3 +0,0 @@
-const Button = ({title, type, onClick}) => <button type={type} onClick={onClick}>{title}</button>
-
-export default Button
--- part3/phonebook/frontend/src/components/Input.jsx
+++
@@ -1,3 +0,0 @@
-const Input = ({name, onChange, value}) => <input name={name} value={value} onChange={onChange}/>
-
-export default Input
