98a7debe9df4b0f11be5357c17a6707e9f65b9af
asweetcake <beka.murat.2021@gmail.com>
2025-08-19 18:57:11 +0500

    Completed ex. 5.1: Blog List Frontend, step 1 - Add initial login functionality that stores user state only in memory - Refactor blogs.js service to use async/await instead of promise chaining

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const MAGIC_STRINGS = {
+    username: 'username',
+    password: 'password'
+  }
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +20,69 @@ const App = () => {
     )  
   }, [])
 
-  return (
+  const handleInput = (event) => {
+    const { name, value } = event.target
+    if (name === MAGIC_STRINGS.username) setUsername(value)
+    else if (name === MAGIC_STRINGS.password) setPassword(value)
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const credentials = { username, password }
+      const authenticatedUser = await loginService.login(credentials)
+      setUser(authenticatedUser)
+      
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.error('Error:', exception)
+    }
+  }
+
+  const loginForm = () => (
+    <div>
+      <h2>Log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input 
+            type='text'
+            name={MAGIC_STRINGS.username}
+            value={username}
+            onChange={handleInput}
+          />
+        </div>
+        <div>
+          password
+          <input 
+            type='password'
+            name={MAGIC_STRINGS.password}
+            value={password}
+            onChange={handleInput}
+          />
+        </div>
+        <button type='submit'>log in</button>
+      </form>
+    </div>
+  )
+
+  const listBlogs = () => (
     <div>
       <h2>blogs</h2>
+      <p>{user.name} logged in</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+    </div>
+  )
+
+  return (
+    <div>
+      { user === null
+          ? loginForm()
+          : listBlogs()
+      }
     </div>
   )
 }
