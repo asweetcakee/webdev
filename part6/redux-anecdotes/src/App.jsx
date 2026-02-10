073233bf604fb29f8440164e57e18780970e8a69
asweetcake <beka.murat.2021@gmail.com>
2026-02-10 21:25:30 +0500

    Completed ex. 6.8: Anecdotes, step 6. Move anecdote rendering list into it's own AnecdoteList component and each list into Anecdote component

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,24 +1,11 @@
-import { useDispatch, useSelector } from 'react-redux'
-import { voteFor } from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
-const App = () => {
-  const dispatch = useDispatch()
-  const anecdotes = useSelector(state => [...state].sort((a, b) => b.votes - a.votes))
-  
+const App = () => {  
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => dispatch(voteFor(anecdote.id))}>vote</button>
-          </div>
-        </div>
-      ))}
-      <h2>create new</h2>
+      <AnecdoteList />
       <AnecdoteForm />
     </div>
   )
