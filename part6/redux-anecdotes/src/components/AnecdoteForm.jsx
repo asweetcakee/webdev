8393b0e96640e8f99e190071fd9f7f302b5a02d0
asweetcake <beka.murat.2021@gmail.com>
2026-02-24 17:45:05 +0500

    Completed .ex 6.15 Anecdotes and the Backend, step 2. Implement createNew anecdote POST method

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,16 +1,18 @@
 import { useDispatch } from "react-redux"
 import { createAnecdote } from "../reducers/anecdoteReducer"
 import { setNotification } from "../reducers/notificationReducer"
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async(event) => {
     event.preventDefault()
     const value = event.target.anecdote.value
     event.target.anecdote.value = ''
-    dispatch(createAnecdote(value))
-    dispatch(setNotification(`You added "${value}"`))
+    const newAnecdote = await anecdoteService.createNew(value)
+    dispatch(createAnecdote(newAnecdote))
+    dispatch(setNotification(`You added "${ newAnecdote.content }"`))
   }
 
   return (
