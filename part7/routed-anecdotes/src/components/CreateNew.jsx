432e294e2523752aac77af2a87c41e80d69ad640
asweetcake <beka.murat.2021@gmail.com>
2026-05-03 17:55:05 +0500

    Completed ex. 7.3: Fixing the spread issue. Refactor input arguments to follow spread syntax format

--- part7/routed-anecdotes/src/components/CreateNew.jsx
+++ part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -3,9 +3,9 @@ import { useNavigate } from 'react-router-dom'
 import { useField } from '../hooks'
 
 const CreateNew = ({ addAnecdote }) => {
-  const content = useField('text')
-  const author = useField('text')
-  const info = useField('text')
+  const { reset: resetContent, ...content } = useField('text')
+  const { reset: resetAuthor, ...author } = useField('text')
+  const { reset: resetInfo, ...info } = useField('text')
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
@@ -13,16 +13,16 @@ const CreateNew = ({ addAnecdote }) => {
     addAnecdote({ 
       content: content.value, 
       author: author.value, 
-      info: info.value, 
+      info: info.value,
       votes: 0 
     })
     navigate('/')
   }
 
   const handleReset = () => {
-    content.reset()
-    author.reset()
-    info.reset()
+    resetContent()
+    resetAuthor()
+    resetInfo()
   }
 
   return (
@@ -31,15 +31,15 @@ const CreateNew = ({ addAnecdote }) => {
       <form onSubmit={handleSubmit} onReset={handleReset}>
         <div>
           content
-          <input name={content.type} value={content.value} onChange={content.onChange} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name={author.type} value={author.value} onChange={author.onChange} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name={info.type} value={info.value} onChange={info.onChange} />
+          <input {...info} />
         </div>
         <button>create</button>
         <button type='reset'>reset</button>
