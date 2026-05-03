246ffbccdeb4bf6720cbfc41f1d6ee7f39305e71
asweetcake <beka.murat.2021@gmail.com>
2026-05-03 18:12:47 +0500

    Completed ex. 7.4: useAnecdotes, step1. Implement custom hook useAnecdotes to load data from backend

--- part7/routed-anecdotes/src/services/anecdotes.js
+++ part7/routed-anecdotes/src/services/anecdotes.js
@@ -2,7 +2,7 @@ const baseUrl = 'http://localhost:3001/anecdotes'
 
 const getAll = async () => {
   const response = await fetch(baseUrl)
-
+  
   if (!response.ok) {
     throw new Error('Failed to fetch notes')
   }
